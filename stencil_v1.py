10b0a089701efd192f4dabc720282794546b3c04
saidctb <said-ctb@live.fr>
2022-02-16 19:55:30 +0100

    add new dot versions

--- stencil_v1.py
+++ stencil_v1.py
@@ -1,20 +1,23 @@
+from numpy import empty
 # ==========================================================================
 def dot_1(mat: "float[:,:,:,:,:,:]", x: "float[:,:,:]", out: "float[:,:,:]",
         n1: int, n2: int, n3: int,
         p1: int, p2: int, p3: int):
 
     #$ omp parallel default(private) shared(mat,x,out) firstprivate( n1, n2, n3)
+    xx = empty((3,n2+3,n3+3))
 
-    #$ omp for schedule(static) collapse(3) nowait
+    #$ omp for schedule(static) nowait
     for i1 in range(0, n1, 1):
+        xx[:,:,:] = x[i1:i1+3,:,:]
         for i2 in range(0, n2, 1):
             for i3 in range(0, n3, 1):
                 v = 0.0
                 for k1 in range(0, 3, 1):
                     for k2 in range(0, 3, 1):
                         for k3 in range(0, 3, 1):
-                            v += mat[1 + i1,1 + i2,1 + i3,k1,k2,k3]*x[i1 + k1,i2 + k2,i3 + k3]
-                out[1 + i1,1 + i2,1 + i3] = v
+                            v += mat[p1 + i1,p2 + i2,p3 + i3,k1,k2,k3]*xx[k1,i2+k2,i3+k3]
+                out[p1 + i1,p2 + i2,p3 + i3] = v
 
     #$ omp end parallel
     return
@@ -23,76 +26,81 @@ def dot_2(mat: "float[:,:,:,:,:,:]", x: "float[:,:,:]", out: "float[:,:,:]",
         p1: int, p2: int, p3: int):
 
     #$ omp parallel default(private) shared(mat,x,out) firstprivate( n1, n2, n3)
+    xx = empty((5,n2+5,n3+5))
 
-    #$ omp for schedule(static) collapse(3) nowait
+    #$ omp for schedule(static) nowait
     for i1 in range(0, n1, 1):
+        xx[:,:,:] = x[i1:i1+5,:,:]
         for i2 in range(0, n2, 1):
             for i3 in range(0, n3, 1):
                 v = 0.0
                 for k1 in range(0, 5, 1):
                     for k2 in range(0, 5, 1):
                         for k3 in range(0, 5, 1):
-                            v += mat[2 + i1,2 + i2,2 + i3,k1,k2,k3]*x[i1 + k1,i2 + k2,i3 + k3]
-                out[2 + i1,2 + i2,2 + i3] = v
+                            v += mat[p1 + i1,p2 + i2,p3 + i3,k1,k2,k3]*xx[k1,i2+k2,i3+k3]
+                out[p1 + i1,p2 + i2,p3 + i3] = v
 
     #$ omp end parallel
     return
-
 def dot_3(mat: "float[:,:,:,:,:,:]", x: "float[:,:,:]", out: "float[:,:,:]",
         n1: int, n2: int, n3: int,
         p1: int, p2: int, p3: int):
 
     #$ omp parallel default(private) shared(mat,x,out) firstprivate( n1, n2, n3)
+    xx = empty((7,n2+7,n3+7))
 
-    #$ omp for schedule(static) collapse(3) nowait
+    #$ omp for schedule(static) nowait
     for i1 in range(0, n1, 1):
+        xx[:,:,:] = x[i1:i1+7,:,:]
         for i2 in range(0, n2, 1):
             for i3 in range(0, n3, 1):
                 v = 0.0
                 for k1 in range(0, 7, 1):
                     for k2 in range(0, 7, 1):
                         for k3 in range(0, 7, 1):
-                            v += mat[3 + i1,3 + i2,3 + i3,k1,k2,k3]*x[i1 + k1,i2 + k2,i3 + k3]
-                out[3 + i1,3 + i2,3 + i3] = v
+                            v += mat[p1 + i1,p2 + i2,p3 + i3,k1,k2,k3]*xx[k1,i2+k2,i3+k3]
+                out[p1 + i1,p2 + i2,p3 + i3] = v
 
     #$ omp end parallel
     return
-
 def dot_4(mat: "float[:,:,:,:,:,:]", x: "float[:,:,:]", out: "float[:,:,:]",
         n1: int, n2: int, n3: int,
         p1: int, p2: int, p3: int):
 
     #$ omp parallel default(private) shared(mat,x,out) firstprivate( n1, n2, n3)
+    xx = empty((9,n2+9,n3+9))
 
-    #$ omp for schedule(static) collapse(3) nowait
+    #$ omp for schedule(static) nowait
     for i1 in range(0, n1, 1):
+        xx[:,:,:] = x[i1:i1+9,:,:]
         for i2 in range(0, n2, 1):
             for i3 in range(0, n3, 1):
                 v = 0.0
                 for k1 in range(0, 9, 1):
                     for k2 in range(0, 9, 1):
                         for k3 in range(0, 9, 1):
-                            v += mat[4 + i1,4 + i2,4 + i3,k1,k2,k3]*x[i1 + k1,i2 + k2,i3 + k3]
-                out[4 + i1,4 + i2,4 + i3] = v
+                            v += mat[p1 + i1,p2 + i2,p3 + i3,k1,k2,k3]*xx[k1,i2+k2,i3+k3]
+                out[p1 + i1,p2 + i2,p3 + i3] = v
 
     #$ omp end parallel
     return
-
 def dot_5(mat: "float[:,:,:,:,:,:]", x: "float[:,:,:]", out: "float[:,:,:]",
         n1: int, n2: int, n3: int,
         p1: int, p2: int, p3: int):
 
     #$ omp parallel default(private) shared(mat,x,out) firstprivate( n1, n2, n3)
+    xx = empty((11,n2+11,n3+11))
 
-    #$ omp for schedule(static) collapse(3) nowait
+    #$ omp for schedule(static) nowait
     for i1 in range(0, n1, 1):
+        xx[:,:,:] = x[i1:i1+11,:,:]
         for i2 in range(0, n2, 1):
             for i3 in range(0, n3, 1):
                 v = 0.0
                 for k1 in range(0, 11, 1):
                     for k2 in range(0, 11, 1):
                         for k3 in range(0, 11, 1):
-                            v += mat[5 + i1,5 + i2,5 + i3,k1,k2,k3]*x[i1 + k1,i2 + k2,i3 + k3]
-                out[5 + i1,5 + i2,5 + i3] = v
+                            v += mat[p1 + i1,p2 + i2,p3 + i3,k1,k2,k3]*xx[k1,i2+k2,i3+k3]
+                out[p1 + i1,p2 + i2,p3 + i3] = v
     #$ omp end parallel
     return
--- test_stencil.py
+++ test_stencil.py
@@ -1,10 +1,60 @@
 import time
 import numpy as np
 
-from stencil_v0 import dot as dot_v0
+from stencil_v0 import dot_1 as dot_1_v0, dot_2 as dot_2_v0, dot_3 as dot_3_v0, dot_4 as dot_4_v0, dot_5 as dot_5_v0
+from stencil_v1 import dot_1 as dot_1_v1, dot_2 as dot_2_v1, dot_3 as dot_3_v1, dot_4 as dot_4_v1, dot_5 as dot_5_v1
+from stencil_v2 import dot_1 as dot_1_v2, dot_2 as dot_2_v2, dot_3 as dot_3_v2, dot_4 as dot_4_v2, dot_5 as dot_5_v2
 
+def select_dot_v0(p):
+    if p==1:
+        return dot_1_v0
+    elif p==2:
+        return dot_2_v0
+    elif p==3:
+        return dot_3_v0
+    elif p==4:
+        return dot_4_v0
+    elif p==5:
+        return dot_5_v0
+    else:
+        raise ValueError("not available")
+
+def select_dot_v1(p):
+    if p==1:
+        return dot_1_v1
+    elif p==2:
+        return dot_2_v1
+    elif p==3:
+        return dot_3_v1
+    elif p==4:
+        return dot_4_v1
+    elif p==5:
+        return dot_5_v1
+    else:
+        raise ValueError("not available")
+
+def select_dot_v2(p):
+    if p==1:
+        return dot_1_v2
+    elif p==2:
+        return dot_2_v2
+    elif p==3:
+        return dot_3_v2
+    elif p==4:
+        return dot_4_v2
+    elif p==5:
+        return dot_5_v2
+    else:
+        raise ValueError("not available")
 # ==============================================================
-def test_1(n1=100, n2=100, n3=100, p1=4, p2=4, p3=4):
+def test_1(n, p):
+    dot_v0 = select_dot_v0(p)
+    dot_v1 = select_dot_v1(p)
+    dot_v2 = select_dot_v2(p)
+
+    p1, p2, p3 = p, p, p
+    n1, n2, n3 = n, n, n
+
     ps = (p1, p2, p3)
     ns = (n1, n2, n3)
     mat = np.random.random((n1+2*p1,n2+2*p2,n3+2*p3 , 2*p1+1, 2*p2+1, 2*p3+1))
@@ -17,9 +67,76 @@ def test_1(n1=100, n2=100, n3=100, p1=4, p2=4, p3=4):
     dot_v0(mat, x, out, *ns, *ps)
     t2 = time.time()
 
-    print('[v0] CPU Time :', t2-t1)
+    time_dot_v0 = t2-t1
     # ...
+
+    # ... version 1
+    out1 = np.zeros_like(x)
+
+    t1 = time.time()
+    dot_v1(mat, x, out1, *ns, *ps)
+    t2 = time.time()
+
+    time_dot_v1 = t2-t1
+    # ...
+
+    # ... version 2
+    out2      = np.zeros_like(x)
+    TT        = 1000
+    block_len = -1
+    for bls in range(1, min(20,n)):
+        if abs(n//bls-n/bls)>1e-10:continue
+
+        out2[:,:,:] = 0.
+        t1 = time.time()
+        dot_v2(mat, x, out2, *ns, *ps, bls)
+        t2 = time.time()
+        T = t2-t1
+
+        if not abs(out-out2).max()<1e-11:
+            print(abs(out-out2).max())
+            raise ValueError("wrong result")
+
+        if T<TT:
+            TT      = T
+            block_len = bls
+
+    time_dot_v2 = TT
+    # ...
+
+#    print('[v0] CPU Time :', time_dot_v0)
+#    print('[v1] CPU Time :', time_dot_v1)
+#    print('[v2] CPU Time :', time_dot_v2, 'for bls:', block_len)
+
+    return time_dot_v0, time_dot_v1, time_dot_v2
 
 ################################################################
 if __name__ == '__main__':
-    test_1()
+    ps    = [1,2,3,4,5]
+    ns    = [10,20,30,40,50,60,70,80,90]
+    nv    = 3
+    times = [np.zeros((len(ps),len(ns))) for i in range(nv)] 
+
+    for i,p in enumerate(ps):
+        for j,n in enumerate(ns):
+            timmings = test_1(n,p)
+            for k,T in enumerate(timmings):
+                times[k][i,j] = T
+
+    from tabulate import tabulate
+    headers = [""] + ["{}**3".format(ni) for ni in ns]
+
+#    for i,T in enumerate(times):
+#        T = T.tolist()
+#        T = [["p={}".format(pi)] + ti for pi,ti in zip(ps,T)]
+#        print("#"*70,"Timming of dot product version {}".format(i), "#"*70)
+#        print(tabulate(T, headers=headers, tablefmt="grid"))
+#        print("\n")
+
+    for i,T in enumerate(times):
+        if i==0:continue
+        T = (times[0]/T).tolist()
+        T = [["p={}".format(pi)] + ti for pi,ti in zip(ps,T)]
+        print("#"*70,"Speedup of dot product version {}".format(i), "#"*70)
+        print(tabulate(T, headers=headers, tablefmt="grid"))
+        print("\n")
